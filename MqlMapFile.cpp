5dd6aac18548d6b42c4a763b048f49f73b73e2bf
appengine8848 <horizon1231@hotmail.com>
2025-11-26 23:53:18 +0800

    Update MqlMapFile.cpp

--- MqlMapFile.cpp
+++ MqlMapFile.cpp
@@ -9,24 +9,24 @@ const char* szName = "Global\\MyTradeSignal"; // 内存映射文件名
 const int BUF_SIZE = 1024; // 缓冲区大小
 
 extern "C" {
-    // 打开/创建内存映射文件
-    EXPORT bool OpenFileMappingA() {
+    // 【已重命名】打开/创建内存映射文件
+    EXPORT bool Mql_OpenFileMapping() {
         if (hMapFile) return true;
 
         hMapFile = CreateFileMappingA(
             INVALID_HANDLE_VALUE,    // use paging file
             NULL,                    // default security
             PAGE_READWRITE,          // read/write access
-            0,                       // maximum object size (high-order DWORD)
-            BUF_SIZE,                // maximum object size (low-order DWORD)
-            szName);                 // name of mapping object
+            0,
+            BUF_SIZE,
+            szName);
 
         if (hMapFile == NULL) {
             return false;
         }
 
-        pBuf = MapViewOfFile(hMapFile,   // handle to map object
-            FILE_MAP_ALL_ACCESS, // read/write permission
+        pBuf = MapViewOfFile(hMapFile,
+            FILE_MAP_ALL_ACCESS,
             0,
             0,
             BUF_SIZE);
@@ -39,15 +39,15 @@ extern "C" {
         return true;
     }
 
-    // 写入数据
-    EXPORT bool WriteToMapFileA(const char* data) {
+    // 【已重命名】写入数据
+    EXPORT bool Mql_WriteToMapFile(const char* data) {
         if (!pBuf) return false;
         CopyMemory((PVOID)pBuf, data, strlen(data) + 1);
         return true;
     }
 
-    // 读取数据
-    EXPORT bool ReadFromMapFileA(char* data, int len) {
+    // 【已重命名】读取数据
+    EXPORT bool Mql_ReadFromMapFile(char* data, int len) {
         if (!pBuf) return false;
         if (len > BUF_SIZE) len = BUF_SIZE;
         CopyMemory((PVOID)data, pBuf, len);
@@ -64,9 +64,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        break;
     case DLL_THREAD_ATTACH:
-        break;
     case DLL_THREAD_DETACH:
         break;
     case DLL_PROCESS_DETACH:
